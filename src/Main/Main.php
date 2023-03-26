82515eaf9fd253b4d099e10b753eccc10d4f006e
HerrSchnecki <107026427+HerrSchnecki@users.noreply.github.com>
2023-03-26 01:06:35 -0700

    Update Main.php

--- src/Main/Main.php
+++ src/Main/Main.php
@@ -1,40 +1,42 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Main;
 
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
-use pocketmine\command\CommandSender;
-use pocketmine\command\Command;
-use pocketmine\Player;
 use pocketmine\utils\TextFormat;
 use jojoe77777\FormAPI\SimpleForm;
 
 class Main extends PluginBase{
 
     public function onEnable(): void {
-        @mkdir($this->getDataFolder());
         $this->saveDefaultConfig();
-        $this->getLogger()->info(TextFormat::GREEN . "Das BroadcastPlugin wurde aktiviert!");
+        $this->getLogger()->info(TextFormat::GREEN . "BroadcastPlugin wurde aktiviert!");
     }
 
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool {
-        if($cmd->getName() == "broadcast"){
-            if($sender instanceof Player){
-                if($sender->hasPermission("broadcast.use")){
-                    $this->openBroadcastForm($sender);
-                    return true;
-                }else{
-                    $sender->sendMessage(TextFormat::RED . "Du hast keine Rechte zu diesem Befehl!");
-                    return true;
-                }
+        if($cmd->getName() === "broadcast"){
+            if(!$sender instanceof Player){
+                $sender->sendMessage(TextFormat::RED . "Dieser Befehl kann nur ingame ausgeführt werden!");
+                return true;
             }
+            if(!$sender->hasPermission("broadcast.use")){
+                $sender->sendMessage(TextFormat::RED . "Du hast keine Rechte diesen Befehl auszuführen!");
+                return true;
+            }
+            $this->openBroadcastForm($sender);
+            return true;
         }
         return false;
     }
 
-    public function openBroadcastForm(Player $player){
-        $form = new SimpleForm(function(Player $player, $data){
+    public function openBroadcastForm(Player $player): void {
+        $form = new SimpleForm(function(Player $player, $data): void {
             if($data !== null){
                 $this->broadcast($player, $data);
             }
@@ -43,10 +45,9 @@ class Main extends PluginBase{
         $form->setContent("Geben Sie eine Nachricht ein, die Sie verbreiten möchten:");
         $form->addInput("", "Beispiel: Willkommen auf dem Server!");
         $form->sendToPlayer($player);
-        return $form;
     }
 
-    public function broadcast(Player $player, $message){
+    public function broadcast(Player $player, string $message): void {
         $config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
         $prefix = $config->get("prefix");
         $broadcastMessage = $prefix . $message;
